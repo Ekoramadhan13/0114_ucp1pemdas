df21758c33e0799985cd36c6958b2257f3394413
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-05 17:19:52 +0700

    UCP 1 pemdas

--- pemdas2904/pemdas2904.cpp
+++ pemdas2904/pemdas2904.cpp
@@ -1,4 +1,58 @@
 #include <iostream>
+using namespace std;
 
 
+int main()
+{
+	float Asroni, Lia, Joko;
+	string status;
+
+	cout << " memasukkan Asroni";
+	cin >> Asroni;
+	cout << "memasukan Lia";
+	cin >> Lia;
+	cout << " masukan Joko";
+	cin >> Joko;
+
+	rerata = (Asroni = Lia = Joko) / 3;
+	if (rerata >= 80) {
+		status = "Diterima";
+	}
+
+	else if (Lia >= 70) {
+		status = "Diterima";
+	}
+
+	else if (Joko <= 70) {
+		status = "Ditolak";
+	}
+
+	else
+	{
+		status = "Diterima";
+		status = "Ditolak";
+	}
+
+	cout << "anda dinyatakan diterima = " << status << endl;
+	cout << "anda dinyatkan ditolak = " << status << endl;
+
+	system("pause");
+	}
+}
+
+
+		
+
+ 
+// 1. variable int
+//	  variable float
+//	  variable char
+
+// 2. if-Else statement digunakan untuk membuat keputusan berdasarkan kondisi yang diberikan. contohnya menjalankan satu blok kode jika kondisi tertentu terpenuhi, dan menjalankan blok kode alternatif jika kondisi tidak terpenuhi.
+// 3. c++ adalah contoh dari implementasi dari struct
+// 4. Prosedur adalah blok kode yang menjalankan serangkaian pernyataan tanpa mengembalikan nilai.
+// contohnya int main
+//	  Fungsi adalah blok kode yang menjalankan serangkaian pernyataan dan mengembalikan nilai ke pemanggil.
+// contohnya int add
+// 5.While loop digunakan untuk menjalankan blok kode selama kondisi tertentu terpenuhi.  
 
